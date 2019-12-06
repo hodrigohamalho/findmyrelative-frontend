781960a954a286ad72f8cb2d4b5a0651cdb9f0ae
Mohammed Saud <msaud@redhat.com>
2019-12-06 17:51:57 +0530

    display shelter location on map

--- src/Display.tsx
+++ src/Display.tsx
@@ -10,10 +10,45 @@ import {
 } from "@patternfly/react-core";
 import mapboxgl from "mapbox-gl";
 import marker from "./icons/marker-red.png";
+import shelter from "./icons/shelter.png";
 
 interface MapDisplayProps {
   position: any;
+  id: string;
+  status: string;
 }
+
+const mapPointLayer = (
+  name: string,
+  lon: string,
+  lat: string,
+  size: number
+): any => {
+  return {
+    id: name,
+    type: "symbol",
+    source: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Point",
+              coordinates: [lon, lat]
+            }
+          }
+        ]
+      }
+    },
+    layout: {
+      "icon-image": name,
+      "icon-size": size
+    }
+  };
+};
 
 const MapDisplay: React.FC<MapDisplayProps> = props => {
   useEffect(() => {
@@ -22,37 +57,34 @@ const MapDisplay: React.FC<MapDisplayProps> = props => {
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11",
       center: [props.position.lon, props.position.lat],
-      zoom: 13
+      zoom: 12
     });
     map.on("load", () => {
       map.loadImage(marker, (err: Error, image: HTMLImageElement) => {
         if (err) throw err;
         map.addImage("marker", image);
-        map.addLayer({
-          id: "points",
-          type: "symbol",
-          source: {
-            type: "geojson",
-            data: {
-              type: "FeatureCollection",
-              features: [
-                {
-                  type: "Feature",
-                  properties: {},
-                  geometry: {
-                    type: "Point",
-                    coordinates: [props.position.lon, props.position.lat]
-                  }
-                }
-              ]
-            }
-          },
-          layout: {
-            "icon-image": "marker",
-            "icon-size": 0.15
-          }
-        });
+        map.addLayer(
+          mapPointLayer("marker", props.position.lon, props.position.lat, 0.15)
+        );
       });
+      if (props.status !== "REPORTED") {
+        map.loadImage(shelter, (err: Error, image: HTMLImageElement) => {
+          if (err) throw err;
+          map.addImage("shelter", image);
+          fetch(process.env.REACT_APP_BACKEND_URL + `/find/shelter/${props.id}`)
+            .then(response => response.json())
+            .then(jsonData => {
+              map.addLayer(
+                mapPointLayer(
+                  "shelter",
+                  jsonData.map.shelter.map.lon,
+                  jsonData.map.shelter.map.lat,
+                  2
+                )
+              );
+            });
+        });
+      }
     });
   });
   return <div id={"map"} style={{ maxWidth: "500px", height: "400px" }}></div>;
@@ -87,6 +119,8 @@ const VictimDetail: React.FC<VictimDetailProps> = props => {
 
           <GridItem span={12}>
             <MapDisplay
+              id={props.data.id}
+              status={props.data.status}
               position={{ lat: props.data.lat, lon: props.data.lon }}
             ></MapDisplay>
           </GridItem>
